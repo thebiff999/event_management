3aa53bb93c6a79e7bd38bd0adf031b003de89644
dh105210 <dh105210@fh-muenster.de>
2020-05-06 14:54:45 +0200

    fix errors in EvenServiceApplicationTest

--- src/test/java/de/fhms/sweng/eventservice/EventServiceApplicationTest.java
+++ src/test/java/de/fhms/sweng/eventservice/EventServiceApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringtBootTest
+@SpringBootTest
 class EventServiceApplicationTest {
     @Test
     void contextLoads(){

a354f925fc6541ad1ddc4d96cb029963a1ed7048
dh105210 <dh105210@fh-muenster.de>
2020-05-06 15:46:20 +0200

    change EventManagementApplication.java

--- src/main/java/de/fhms/sweng/event_management/EventManagementApplication.java
+++ src/main/java/de/fhms/sweng/event_management/EventManagementApplication.java
@@ -1,4 +1,11 @@
 package de.fhms.sweng.event_management;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EventManagementApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(EventManagementApplication.class, args);
+    }
 }

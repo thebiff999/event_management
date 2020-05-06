8c847bf9799b62985abee235103837bfd0992506
dh105210 <dh105210@fh-muenster.de>
2020-05-06 10:58:53 +0200

    updated EventRestController

--- src/main/java/de/fhms/sweng/event_management/rest/EventRestController.java
+++ src/main/java/de/fhms/sweng/event_management/rest/EventRestController.java
@@ -1,4 +1,16 @@
 package de.fhms.sweng.event_management.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/event")
 public class EventRestController {
+
+    @GetMapping("/test")
+    public String getTest() {
+        return "Test";
+    }
 }

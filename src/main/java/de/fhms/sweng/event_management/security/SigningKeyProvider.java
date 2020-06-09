0ad0649f7423fdec2b327eb4c56be9f50fd874ce
dh105210 <dh105210@fh-muenster.de>
2020-06-09 18:04:31 +0200

    added resource tags in pom.xml to solve problems with the jwt keys

--- src/main/java/de/fhms/sweng/event_management/security/SigningKeyProvider.java
+++ src/main/java/de/fhms/sweng/event_management/security/SigningKeyProvider.java
@@ -2,6 +2,8 @@ package de.fhms.sweng.event_management.security;
 
 import de.fhms.sweng.event_management.exceptions.NotAuthorizedException;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
 import org.springframework.stereotype.Component;
@@ -19,26 +21,36 @@ public class SigningKeyProvider {
 
     private PublicKey publicKey;
     private PrivateKey privateKey;
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     @PostConstruct
     protected void loadSigningKeys() {
         try {
             //read privateKey
+            LOGGER.trace("try to read privateKey");
             InputStream inputStream = new ClassPathResource("privateKey").getInputStream();
             byte[] encodedPrivateKey = IOUtils.toByteArray(inputStream);
+            LOGGER.trace("success reading privateKey");
 
             //encode privateKey
+            LOGGER.trace("try to encode privateKey");
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
             PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(encodedPrivateKey);
             this.privateKey = keyFactory.generatePrivate(privateKeySpec);
+            LOGGER.trace("success encoding privateKey");
 
             //read public key
+            LOGGER.trace("try to read publicKey");
             inputStream = new ClassPathResource("publicKey").getInputStream();
             byte[] encodedPublicKey = IOUtils.toByteArray(inputStream);
+            LOGGER.trace("success reading publicKey");
 
             //encode publicKey
+            LOGGER.trace("try to encode publicKey");
             X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(encodedPublicKey);
             this.publicKey = keyFactory.generatePublic(publicKeySpec);
+            LOGGER.trace("success encoding publicKey");
+
         }  catch (Exception e) {
             throw new NotAuthorizedException("Reading keys went wrong. Try again");
         }
@@ -52,5 +64,3 @@ public class SigningKeyProvider {
         return publicKey;
     }
 }
-
-

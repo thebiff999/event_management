37378667657aa9c2e220dde905ba98e33ffaa212
dh105210 <dh105210@fh-muenster.de>
2020-06-15 17:32:46 +0200

    moved EventManagementApplicationTest in to the package. It was just laying in the java folder, can't believe I wasted so much time looking for this stupid error

--- src/test/java/de/fhms/sweng/event_management/EventManagementApplicationTest.java
+++ src/test/java/de/fhms/sweng/event_management/EventManagementApplicationTest.java
@@ -1,3 +1,5 @@
+package de.fhms.sweng.event_management;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;

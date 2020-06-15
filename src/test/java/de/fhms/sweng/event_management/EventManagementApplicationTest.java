22934cb1392139797895523af8e553e1625943f0
dh105210 <dh105210@fh-muenster.de>
2020-06-15 21:39:09 +0200

    changes to properties files in test stage

--- src/test/java/de/fhms/sweng/event_management/EventManagementApplicationTest.java
+++ src/test/java/de/fhms/sweng/event_management/EventManagementApplicationTest.java
@@ -1,6 +1,8 @@
 package de.fhms.sweng.event_management;
 
+import de.fhms.sweng.event_management.consumer.UserConsumer;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 

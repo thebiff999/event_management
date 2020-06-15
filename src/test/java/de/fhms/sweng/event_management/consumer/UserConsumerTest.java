7864493d0d3b41d6cc49d6669bf902c05b30f862
dh105210 <dh105210@fh-muenster.de>
2020-06-16 00:14:35 +0200

    added UserConsumerTest

---
+++ src/test/java/de/fhms/sweng/event_management/consumer/UserConsumerTest.java
@@ -0,0 +1,92 @@
+package de.fhms.sweng.event_management.consumer;
+
+import de.fhms.sweng.event_management.dto.BusinessUserTO;
+import de.fhms.sweng.event_management.producer.EventProducer;
+import de.fhms.sweng.event_management.services.BusinessUserService;
+import de.fhms.sweng.event_management.services.MapperService;
+import org.apache.qpid.server.SystemLauncher;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+public class UserConsumerTest {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private static SystemLauncher systemLauncher = new SystemLauncher();
+    private final static String EXCHANGE = "TestExchange";
+    private final static String KEY = "TestRoutingKey";
+
+    @Value("${amqp.rabbitmq.queue.euser.save}")
+    private String queueString;
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @Autowired
+    private EventProducer eventProducer;
+
+    @Autowired
+    private MapperService mapperService;
+
+    @SpyBean
+    BusinessUserService userService;
+
+    @BeforeEach
+    public void startBroker() throws Exception {
+
+        LOGGER.debug("starting broker");
+
+        Map<String, Object> attributes = new HashMap<>();
+        URL initialConfig = EventProducer.class.getClassLoader().getResource("initial-config.json");
+        attributes.put("initialConfigurationLocation", initialConfig.toExternalForm());
+        attributes.put("type", "Memory");
+        attributes.put("startupLoggedToSystemOut", true);
+        attributes.put("qpid.amqp_port", "5672");
+        systemLauncher.startup(attributes);
+
+        CachingConnectionFactory cf = new CachingConnectionFactory("localhost", 5672);
+        AmqpAdmin admin = new RabbitAdmin(cf);
+        LOGGER.debug("creating exhcange");
+        DirectExchange exchange = new DirectExchange(EXCHANGE);
+        admin.declareExchange(exchange);
+        LOGGER.debug("creating and binding queue1");
+        Queue queue1 = new Queue(queueString, true);
+        admin.declareQueue(queue1);
+        admin.declareBinding(BindingBuilder.bind(queue1).to(exchange).with(KEY));
+        cf.destroy();
+
+    }
+
+
+    @AfterEach
+    public void stopBroker() {
+        systemLauncher.shutdown();
+    }
+
+    @Test
+    void shouldReceiveUser() throws Exception {
+        BusinessUserTO user = new BusinessUserTO();
+        doNothing().when(userService).createBusinessUser(mapperService.convertToUser(user));
+        amqpTemplate.convertAndSend(EXCHANGE, KEY, user);
+        Thread.sleep(5000);
+        verify(userService).createBusinessUser(mapperService.convertToUser(user));
+    }
+
+
+
+}

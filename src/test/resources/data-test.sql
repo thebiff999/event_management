58505758940a2775d78cdfd6203ab1194cac62c4
dh105210 <dh105210@fh-muenster.de>
2020-06-15 18:15:10 +0200

    edited rabbitmq properties for test stage

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,31 +1,20 @@
-INSERT INTO events (id, datetime, description, name, radius, business_user_id, location_id) VALUES
-(01, 01, 'Event1', 'the first event of user1', '2020-06-01 18:00:00', 5, 1),
-(02, 01, 'Event2', 'the second event of user1', '2020-07-01 19:00:00', 3, 2),
-(03, 02, 'Event3', 'the first event of user2', '2020-07-02 20:15:00', 4, 3),
-(04, 02, 'Event4', 'the second event of user2', '2020-06-02 19:30:00', 2, 4);
-
 INSERT INTO business_users VALUES
-(1, 'Test-User Vorname', 'Test-User Nachname', 'alex@fhms.de', 'EUSER');
+(0, 'Test-User Vorname', 'Test-User Nachname', 'mail@test.com', 'EUSER'),
+(1, 'tom', 'mustermann', 'tom@fhms.de', 'EUSER');
 
 INSERT INTO locations (id, latitude, longitude) VALUES
-(1, 50.00, 50.00)
-(2, 60.00, 70.00),
-(3, 70.00, 60.00),
-(4, 80.00, 80.00);
+(1, 50.00, 50.00),
+(2, 60.00, 60.00);
 
 INSERT INTO preferences(id, value) VALUES
 (1,'Music'),
-(2, 'Sport'),
-(3, 'Cars'),
-(4, 'Books');
+(2, 'Party');
+
+INSERT INTO events (id, datetime, description, name, radius, business_user_id, location_id) VALUES
+(1, '2020-04-22T12:30:00', 'This is the description', 'Test Event', 5, 0, 1),
+(2, '2020-06-13T11:00:00', 'Geburtstagsparty im Garten', 'Hans Geburtstag', 2, 1, 2);
 
 INSERT INTO event_preferences (event_id, preference_id) VALUES
 (1,1),
 (2,1),
-(2,2),
-(3,1),
-(3,3),
-(4,1),
-(4,2),
-(4,3),
-(4,4);
+(2,2);

4f42904002806f9c51e50188006d26004ddfb72f
dh105210 <dh105210@fh-muenster.de>
2020-06-21 14:49:06 +0200

    replaced 'insert' with 'replace' statements in the mydata.sql file to avoid duplicate key errors during deployment

--- src/main/resources/mydata.sql
+++ src/main/resources/mydata.sql
@@ -1,20 +1,19 @@
-INSERT INTO events (id, datetime, description, name, radius, business_user_id, location_id) VALUES
+REPLACE INTO events (id, datetime, description, name, radius, business_user_id, location_id) VALUES
     (1, '2020-04-22T12:30:00', 'This is the description', 'Test Event', 5, 0, 1),
     (2, '2020-06-13T11:00:00', 'Geburtstagsparty im Garten', 'Hans Geburtstag', 2, 1, 2);
-
-INSERT INTO business_users VALUES
+REPLACE INTO business_users VALUES
     (0, 'Test-User Vorname', 'Test-User Nachname', 'mail@test.com', 'EUSER'),
     (1, 'tom', 'mustermann', 'tom@fhms.de', 'EUSER');
 
-INSERT INTO locations (id, latitude, longitude) VALUES
+REPLACE INTO locations (id, latitude, longitude) VALUES
     (1, 50.00, 50.00),
     (2, 60.00, 60.00);
 
-INSERT INTO preferences(id, value) VALUES
+REPLACE INTO preferences(id, value) VALUES
     (1,'Music'),
     (2, 'Party');
 
-INSERT INTO event_preferences (event_id, preference_id) VALUES
+REPLACE INTO event_preferences (event_id, preference_id) VALUES
     (1,1),
     (2,1),
     (2,2);
